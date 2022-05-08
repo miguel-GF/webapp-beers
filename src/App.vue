5bd2c2e05236f12e963566be6dd26087e8120602
miguel-GF <miguel.gf.130@gmail.com>
2022-05-07 23:24:57 -0500

    [App] Se pasa header a componente

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <router-link to="/">
-          <v-img
-            alt="Vuetify Logo"
-            class="shrink mr-2"
-            contain
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-            transition="scale-transition"
-            width="40"
-          />
-        </router-link>
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        elevation="2"
-        small
-        color="secondary"
-        to="/Beers"
-      >
-        Beers
-        <v-icon class="ml-2">
-          mdi-glass-mug-variant
-        </v-icon>
-      </v-btn>
-
-    </v-app-bar>
-
+    <app-bar />
     <loader
       :mostrar="$store.getters.getLoader"
       :mensaje="'Getting beers ..'"
     />
-
     <v-main>
       <router-view/>
     </v-main>
@@ -46,9 +12,12 @@
 </template>
 
 <script>
+import AppBar from './components/AppBar'
 export default {
   name: 'App',
-
+  components: {
+    AppBar
+  },
   data () {
     return {}
   }
---
+++ src/components/AppBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <router-link to="/">
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"
+          />
+        </router-link>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        elevation="2"
+        small
+        color="secondary"
+        to="/Beers"
+      >
+        Beers
+        <v-icon class="ml-2">
+          mdi-glass-mug-variant
+        </v-icon>
+      </v-btn>
+
+    </v-app-bar>
+</template>
