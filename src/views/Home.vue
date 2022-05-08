b739afbd05fdaf3f60844f7b9ca96b7db933ffc4
miguel-GF <miguel.gf.130@gmail.com>
2022-05-07 21:52:41 -0500

    [Home] Se actualiza home y se quita vista de prueba

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
   <v-container>
-    <v-row class="text-center vertical-align">
+    <v-row class="text-center">
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Home
+          Welcome
         </h1>
+        <v-btn
+          elevation="2"
+          small
+          color="secondary"
+          to="/Beers"
+        >
+          Go to see our Beers!
+          <v-icon class="ml-2">
+            mdi-glass-mug-variant
+          </v-icon>
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
